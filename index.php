8adbeb2c66ec4c57f8cc6f3477de3a6058f51125
Dana <danacanorodriguez@gmail.com>
2021-11-28 00:22:48 +0100

    A

--- index.php
+++ index.php
@@ -1,30 +1,75 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <title>Commentaires</title>
-    <link rel="stylesheet" href="css/style.css" />
-    <script type="application/javascript" src="js/script.js"></script>
-</head>
-<body>
-
-<h1>Commentaires</h1>
-
-<?php echo 'toto'; ?>
-
 <?php
-$mysqli = new mysqli("localhost","root","","commentaires");
+//test si mon formulaire est bien renseigné
+if(!empty($_POST["email"]) && !empty($_POST["commentaire"]))
+{
+    //je récupère les données de mon formulaire dans des variables
+    $emaildemonform = $_POST["email"];
+    $commentdemonform = $_POST["commentaire"];
+    $objetdemonform = $_POST["objet"];
+    // Connection BDD
+    $db = mysqli_connect("localhost","root","","commentaires");
 
-// Check connection
-if ($mysqli -> connect_errno) {
-    echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
-    exit();
+    //requete sql d'insertion dans la bdd
+    $sql = 'INSERT INTO commentaire(commentaire_email,commentaire_contenu, commentaire_objet) VALUES ("'.$emaildemonform.'","'.$commentdemonform.'","'.$objetdemonform.'")';
+
+    //exécution de la requete sql d'insertion dans la bdd
+    mysqli_query($db,$sql);
 }
+//sinon j'affiche une erreur mais faut mieux pas le faire car sinon elle s'affichera pour tout le monde au premier chargement
 else
-    echo 'succes';
+    //echo 'erreur';
+
+
+//dans tous les cas, j'affiche le reste de ma page
 ?>
 
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <title>Commentaires</title>
+        <link rel="stylesheet" href="css/style.css" />
+        <script type="application/javascript" src="js/script.js"></script>
+    </head>
+    <body>
+        <h1>Commentaires</h1>
 
+        <?php
 
+        // Connection BDD
+        $db = mysqli_connect("localhost","root","","commentaires");
+
+        // requete
+        $sql = "select * from commentaire";
+
+        // Execution de la requete
+        $res = mysqli_query($db,$sql);
+
+        // début Boucle qui permet d'afficher les commentaires
+        // mysqli_fetch_array : interroge la ligne suivante
+        while($ligne = mysqli_fetch_array($res))
+        {
+            echo $ligne["commentaire_objet"].'<br>';
+            echo $ligne["commentaire_contenu"].'<br>';
+            echo 'date :'.$ligne["commentaire_date"].' écrit par : '.$ligne["commentaire_email"];
+            echo '<br><br>'  ;
+        }
+        // Fin de la boucle
+
+        // fermeture bdd
+        mysqli_close($db);
+
+        ?>
+
+    <form method="post" action="index.php">
+        <label for="email">Votre adresse e-mail</label><br />
+        <input required type="email" name="email" id="email" placeholder="Votre adresse e-mail" size="30" maxlength="100" /><br>
+        <label for="objet">Votre Objet</label><br />
+        <input required type="objet" name="objet" id="objet" placeholder="Votre Objet" size="30" maxlength="100" /><br>
+        <label for="commentaire">Merci de rensigner votre commentaire</label><br />
+        <textarea name="commentaire" id="commentaire" placeholder= "Commentaire" rows="10" cols="50" maxlength="5000"></textarea>
+        <br />
+        <input type="submit" value="Envoyer" name="submit_envoyer"/>
+    </form>
 </body>
 </html>
--- projet.md
+++ projet.md
@@ -1,20 +1,53 @@
-## Título
-### Subtítulo
-Este es un ejemplo de texto que da entrada a una lista genérica de elementos:
+*** Markdown ***
+*******************
+## Le Markdown
+### Une petit démonstration de Markdown avec HTML
 
-- Elemento 1
-- Elemento 2
-- Elemento 3
+Voici les exemples de l'id commentaires du projet en cours en PHP + MySQL:
 
-Este es un ejemplo de texto que da entrada a una lista numerada:
+- id 1
+- id 2
+- id 3
+- id 4
 
-1. Elemento 1
-2. Elemento 2
-3. Elemento 3
+Voici les exemples du contenu des commentaires:
+
+1. Comentaire 1
+2. Comentaire 2
+3. Comentaire 3
+4. Comentaire 4
 
 Al texto en Markdown puedes añadirle formato como **negrita** o *cursiva* de una manera muy sencilla.
 
 ~~~
-Creando códigos de bloque.
-Puedes añadir tantas líneas y párrafos como quieras.  
+Vous pouvez créer des codes de bloc et créer autant 
+de lignes et de paragraphes que vous le souhaitez.
+Il est également possible de créer des tableaux ↓  
 ~~~
+commentaire_id | comentaire_contenu
+------------- | ------------
+1       | Comment1
+2       | Comment2   
+
+*** Tableaux HTML ***
+*******************
+
+<table>
+<thead>
+<tr>
+  <th>commentaire_id</th>
+  <th>comentaire_contenu</th>
+</tr>
+</thead>
+<tbody>
+<tr>
+  <td>1</td>
+  <td>Comment1</td>
+</tr>
+<tr>
+  <td>2</td>
+  <td>Comment2</td>
+</tr>
+</tbody>
+</table>
+
